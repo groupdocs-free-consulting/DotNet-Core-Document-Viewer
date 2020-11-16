0ca9d564c048b07a3c3dd77fe4aa41080cb1e2cc
atirtahirgroupdocs <=>
2020-11-16 15:49:08 +0500

    application added

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# DotNet-Core-Document-Viewer
+# Render a Document in Razor View
+This .NET Core web application will elaborate how GroupDocs.Viewer for .NET [API](https://products.groupdocs.com/viewer/net) renders a document in browser. It takes a source file as input, and renders it in Razor View. 
+
+### How to Run
+* Download or clone the project
+* Add GroupDocs.Viewer for .NET DLL/Install NuGet
+* Build and run solution
+
+If you don't have a license. You can request a [temporary license](https://purchase.groupdocs.com/buy) in order to evaluate these proejcts without any API limitation. 
+
+## Screenshots
+* Sample Word document in MS Word
+![](Screenshots/word.PNG)
+* Sample PDF displayed using ASP.NET Core API application
+![](Screenshots/output.PNG)
+
+# Interested in GroupDocs free consulting project?
+[If you are also interested in a free consulting project by GroupDocs team then please view details on this page](https://github.com/groupdocs-free-consulting/)
+
+If you have any questions about GroupDocs APIs, please feel free to post your query in [GroupDocs Forum](https://forum.groupdocs.com/).
+
+Also, you can keep in touch with the latest developments in file format APIs offered by GroupDocs at our [Blog](https://blog.groupdocs.com/).
+
+# This free consulting project is based on the following issues:
+* [DotNet Core 3.1 App that includes downloading files from Azure Storage to be viewed within the application](https://github.com/groupdocs-free-consulting/projects/issues/6) 
+
+
+
+
---
+++ ViewerSampleApplication/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupDocs.Viewer;
+using GroupDocs.Viewer.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ViewerSampleApplication.Models;
+
+namespace ViewerSampleApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            License lic = new License();
+            lic.SetLicense(@"D:/GroupDocs.Total.NET.lic");
+
+            string outputDirectory = ("Output/");
+            string outputFilePath = Path.Combine(outputDirectory, "output.pdf");
+
+            using (Viewer viewer = new Viewer("SourceDocuments/sample.docx"))
+            {
+                PdfViewOptions options = new PdfViewOptions(outputFilePath);
+                viewer.View(options);
+            }
+            var fileStream = new FileStream("Output/" + "output.pdf",
+                                    FileMode.Open,
+                                    FileAccess.Read
+                                  );
+            var fsResult = new FileStreamResult(fileStream, "application/pdf");
+            ViewBag.filePath = "Output/output.pdf";
+            return View();
+        }
+        public ActionResult RenderFile()
+        {
+            var fileStream = new FileStream("Output/" + "output.pdf",
+                                    FileMode.Open,
+                                    FileAccess.Read
+                                  );  
+            return File(fileStream, "application/pdf");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
